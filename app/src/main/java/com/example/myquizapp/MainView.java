3dc468c4573d8c53ec98af2fb4a1d0cf50440e54
Abubakar Sohail <abubakarsohail83@gmail.com>
2022-11-13 23:36:24 +0500

    Adding the random question module

--- app/src/main/java/com/example/myquizapp/MainView.java
+++ app/src/main/java/com/example/myquizapp/MainView.java
@@ -1,11 +1,21 @@
 package com.example.myquizapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 
-public class MainView extends AppCompatActivity {
+public class MainView extends AppCompatActivity implements View.OnClickListener{
+
+    public static String question[]={
+            "Who was the first caliph of Islam?","Which Sahabi was given the title of Sword Of Allah","In which Gazwa did Prophet Muhammad's(Peace be upon him) tooth break?","In which sura of Quran did Allah mention the rulings about Hijaab?"
+    };
+    public static String choices[][]={{"Hazrat Abu Bakr Sidduiq(R.A.)","Hazrat Omar R.A.","Hazrat Ali R.A.","Hazrat Usman R.A."},{"Hazrat Khalid Bin Waleed R.A.","Hazrat Abu Obaida R.A.","Hazrat Ali R.A.","Hazrat Omar R.A."},{"Gazwa Khandaq","Gazwa Badar","Gazwa Khaibar","Gazwa Ohad"},{"Sura Baqarah","Sura Nisa","Sura Anfaal","Sura Ahzaab"}};
+
+    public static String correctAnswers[]={"Hazrat Abu Bakr Sidduiq(R.A.)","Hazrat Khalid Bin Waleed R.A.","Gazawa Ohad","Sura Ahzaab"};
+
+    int score=0;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
 
@@ -22,18 +32,27 @@ public class MainView extends AppCompatActivity {
         opt2=findViewById(R.id.optB);
         opt3=findViewById(R.id.optC);
         opt4=findViewById(R.id.optD);
+        submit =findViewById(R.id.submit);
+
+        opt1.setOnClickListener(this);
+        opt2.setOnClickListener(this);
+        opt3.setOnClickListener(this);
+        opt4.setOnClickListener(this);
+        submit.setOnClickListener(this);
+
+        noOfQuestions.setText("Total Questions : "+question.length);
+        int questionIndArr[];
+        for(int i=0;i<question.length;i++)
+        {
+            showNewQuestion();
+        }
+
     }
 
+    @Override
+    public void onClick(View view) {
 
-
-
-
-public static String question[]={
-"Who was the first caliph of Islam?","Which Sahabi was given the title of Sword Of Allah","In which Gazwa did Prophet Muhammad's(Peace be upon him) tooth break?","In which sura of Quran did Allah mention the rulings about Hijaab?"
-};
-    public static String choices[][]={{"Hazrat Abu Bakr Sidduiq(R.A.)","Hazrat Omar R.A.","Hazrat Ali R.A.","Hazrat Usman R.A."},{"Hazrat Khalid Bin Waleed R.A.","Hazrat Abu Obaida R.A.","Hazrat Ali R.A.","Hazrat Omar R.A."},{"Gazwa Khandaq","Gazwa Badar","Gazwa Khaibar","Gazwa Ohad"},{"Sura Baqarah","Sura Nisa","Sura Anfaal","Sura Ahzaab"}};
-
-    public static String correctAnswers[]={"Hazrat Abu Bakr Sidduiq(R.A.)","Hazrat Khalid Bin Waleed R.A.","Gazawa Ohad","Sura Ahzaab"};
+    }
 }
 
 
