b56030bfccf39f025a30edb8042879610fdf3ff6
Abubakar Sohail <abubakarsohail83@gmail.com>
2022-11-13 22:40:48 +0500

    added some basic oncreate function and classes in main

--- app/src/main/java/com/example/myquizapp/MainView.java
+++ app/src/main/java/com/example/myquizapp/MainView.java
@@ -1,4 +1,43 @@
 package com.example.myquizapp;
 
-public class MainView {
-}
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.view.View;
+
+
+import android.widget.Button;
+
+
+import android.widget.CheckBox;
+
+
+import android.widget.RadioButton;
+
+
+import android.widget.TextView;
+
+
+
+
+import java.util.ArrayList;
+
+
+import java.util.Collections;
+
+
+import java.util.Random;
+
+
+public class MainView extends AppCompatActivity {
+
+    @Override
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.layout);
+            }}
